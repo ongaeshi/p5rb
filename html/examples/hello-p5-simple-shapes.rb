eee910b4e3b96367bf305eff85e5c3b211bfedc1
ongaeshi <ongaeshi0621@gmail.com>
2023-02-15 03:30:13 +0900

    feat: Call p3 methods from ruby

--- html/examples/hello-p5-simple-shapes.rb
+++ html/examples/hello-p5-simple-shapes.rb
@@ -1,1 +1,56 @@
-puts "Hello, world!"
+# require 'js'
+
+# def setup
+#   p "setup"
+#   # Create the canvas
+#   createCanvas(720, 400)
+#   background(200)
+
+#   # Set colors
+#   fill(204, 101, 192, 127)
+#   stroke(127, 63, 120)
+
+#   # A rectangle
+#   rect(40, 120, 120, 40)
+#   # An ellipse
+#   ellipse(240, 240, 80, 80)
+#   # A triangle
+#   triangle(300, 100, 320, 100, 310, 80)
+
+#   # # A design for a simple flower
+#   # translate(580, 200)
+#   # noStroke()
+#   # 10.times do
+#   #   ellipse(0, 30, 20, 80)
+#   #   rotate(PI/5)
+#   # end
+# end
+
+# JS.global[:setup] = -> {
+#   setup
+# }
+
+require 'js'
+
+p5 = JS.global[:p5]
+window = JS.global[:window]
+document = window[:document]
+
+sketch = ->(p) {
+  x = 100
+  y = 100
+
+  p[:setup] = -> {
+    p.createCanvas(700, 410)
+  }
+
+  p[:draw] = -> {
+    p.background(0)
+    p.fill(255)
+    p.rect(x, y, 50, 50)
+  }
+}
+
+container = document.querySelector('main')
+container[:innerHTML] = ''
+window[:constructors].p5(sketch, container)

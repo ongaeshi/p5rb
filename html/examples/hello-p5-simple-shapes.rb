07a1012823da5b563ec546e62e29192a18f08a82
ongaeshi <ongaeshi0621@gmail.com>
2023-02-15 04:09:31 +0900

    feat: Port hello-p5-simple-shapes

--- html/examples/hello-p5-simple-shapes.rb
+++ html/examples/hello-p5-simple-shapes.rb
@@ -1,43 +1,27 @@
-# require 'js'
-
-# def setup
-#   p "setup"
-#   # Create the canvas
-#   createCanvas(720, 400)
-#   background(200)
-
-#   # Set colors
-#   fill(204, 101, 192, 127)
-#   stroke(127, 63, 120)
-
-#   # A rectangle
-#   rect(40, 120, 120, 40)
-#   # An ellipse
-#   ellipse(240, 240, 80, 80)
-#   # A triangle
-#   triangle(300, 100, 320, 100, 310, 80)
-
-#   # # A design for a simple flower
-#   # translate(580, 200)
-#   # noStroke()
-#   # 10.times do
-#   #   ellipse(0, 30, 20, 80)
-#   #   rotate(PI/5)
-#   # end
-# end
-
-# JS.global[:setup] = -> {
-#   setup
-# }
-
 def setup
-  createCanvas(700, 410)
+  # Create the canvas
+  createCanvas(720, 400)
+  background(200)
+
+  # Set colors
+  fill(204, 101, 192, 127)
+  stroke(127, 63, 120)
+
+  # A rectangle
+  rect(40, 120, 120, 40)
+  # An ellipse
+  ellipse(240, 240, 80, 80)
+  # A triangle
+  triangle(300, 100, 320, 100, 310, 80)
+
+  # A design for a simple flower
+  translate(580, 200)
+  noStroke()
+  10.times do
+    ellipse(0, 30, 20, 80)
+    rotate(1) # PI/5 (TODO: Floating point cannot be passed as an argument?) 
+  end
 end
 
 def draw
-  x = 100
-  y = 100  
-  background(0)
-  fill(255)
-  rect(x, y, 50, 50)
 end

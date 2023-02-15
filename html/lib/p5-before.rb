729e7c42262f40628a80a82f55be00c232f455da
ongaeshi <ongaeshi0621@gmail.com>
2023-02-16 03:00:21 +0900

    fix: Cannot call method <script>?

--- html/lib/p5-before.rb
+++ html/lib/p5-before.rb
@@ -1,5 +1,9 @@
 $p = nil
 
+# def width = $p[:width].to_i
+# def height = $p[:height].to_i
+# def random(*args) = $p.call(:random, *args).to_f
+
 def method_missing(name, *args)
   if $p.respond_to?(name)
     $p.call(name, *args)
@@ -8,7 +12,3 @@ def method_missing(name, *args)
   end
 end
 
-def width = $p[:width].to_i
-def height = $p[:height].to_i
-def random(*args) = $p.call(:random, *args).to_f
- 

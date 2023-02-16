2d74a1c0a1caf9afaffa2ab5ddfa1b40d65b4b40
ongaeshi <ongaeshi0621@gmail.com>
2023-02-16 10:53:43 +0900

    feat: Add Comments/Statements, Coordinates

---
+++ docs/examples/structure-comments-and-statements.rb
@@ -0,0 +1,14 @@
+# The createCanvas function is a statement that tells the computer 
+# how large to make the window.
+# Each function statement has zero or more parameters. 
+# Parameters are data passed into the function
+# and are used as values for telling the computer what to do.
+def setup
+   createCanvas(710, 400)
+end
+
+# The background function is a statement that tells the computer
+# which color (or gray value) to make the background of the display window 
+def draw
+   background(204, 153, 0)
+end

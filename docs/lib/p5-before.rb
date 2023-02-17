56e2cda559e820e71b74c732de4def9914e0e9a2
ongaeshi <ongaeshi0621@gmail.com>
2023-02-18 00:23:06 +0900

    refactor: JS::Object.to_r を抽出

--- docs/lib/p5-before.rb
+++ docs/lib/p5-before.rb
@@ -20,19 +20,22 @@ def mouseX = $p[:mouseX].to_f
 def mouseY = $p[:mouseY].to_f
 def createSlider(*args) = Element.new($p.call(:createSlider, *args))
 
+class JS::Object
+  def to_r
+    case ret.typeof
+    when "number"
+      ret.to_f
+    when "string"
+      ret.to_s
+    else
+      ret
+    end
+  end
+end
+
 def method_missing(name, *args)
   if $p.respond_to?(name)
-    ret = $p.call(name, *args)
-    if ret.is_a?(JS::Object)
-      case ret.typeof
-      when "number"
-        ret.to_f
-      when "string"
-        ret.to_s
-      else
-        ret
-      end
-    end
+    $p.call(name, *args).to_r
   else
     super
   end

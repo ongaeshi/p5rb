31eb6f82384442607f54f4d53132c7c6021c9c06
ongaeshi <ongaeshi0621@gmail.com>
2023-02-18 18:37:37 +0900

    refactor: Fix

--- docs/lib/p5-after.rb
+++ docs/lib/p5-after.rb
@@ -1,7 +1,3 @@
-p5 = JS.global[:p5]
-window = JS.global[:window]
-document = window[:document]
-
 sketch = ->(p) {
   $p = p  
   $p[:setup] = -> { setup } if defined?(setup)
@@ -9,6 +5,6 @@ sketch = ->(p) {
   $p[:mousePressed] = ->(e) { mousePressed } if defined?(mousePressed)
 }
 
-container = document.querySelector('main')
-container[:innerHTML] = ''
-window[:constructors].p5(sketch, container)
+container = JS.global.document.querySelector('main')
+container.innerHTML = ""
+JS.global.window.constructors.p5(sketch, container)

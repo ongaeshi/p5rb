927ae999129c55bbfaab2586424d0ffd82eb43bf
ongaeshi <ongaeshi0621@gmail.com>
2023-02-18 19:46:34 +0900

    feat: Add structure-create-graphics

---
+++ docs/examples/structure-create-graphics/main.rb
@@ -0,0 +1,22 @@
+$pg = nil
+
+def setup
+  createCanvas(710, 400)
+  $pg = createGraphics(400, 250)
+end
+
+def draw
+  fill(0, 12)
+  rect(0, 0, width, height)
+  fill(255)
+  noStroke()
+  ellipse(mouseX, mouseY, 60, 60)
+
+  $pg.background(51)
+  $pg.noFill()
+  $pg.stroke(255)
+  $pg.ellipse(mouseX - 150, mouseY - 75, 60, 60)
+
+  #Draw the offscreen buffer to the screen with image()
+  image($pg, 150, 75)
+end

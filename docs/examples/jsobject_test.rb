8d2fe0fa918a0f43b4936dc0e9ebc40a970e6f61
ongaeshi <ongaeshi0621@gmail.com>
2023-02-18 14:12:29 +0900

    feat: これで上書きできる

--- docs/examples/jsobject_test.rb
+++ docs/examples/jsobject_test.rb
@@ -1,5 +1,36 @@
 require "js"
 
+class JS::Object
+  def method_missing(sym, *args, &block)
+    ret = self[sym]
+  
+    case ret.typeof
+    when "function"
+      self.call(sym, *args, &block).to_r
+    when "undefined"
+      super
+    else
+      ret.to_r
+    end
+  end
+
+  def respond_to_missing?(sym, include_private)
+    return true if super
+    self[sym].typeof != "undefined"
+  end
+
+  def to_r
+    case self.typeof
+    when "number"
+      self.to_f
+    when "string"
+      self.to_s
+    else
+      self
+    end
+  end
+end
+
 p 1
 
 document = JS.global[:document]
@@ -24,6 +55,8 @@ main[:innerHTML] = <<~HTML
 HTML
 
 p main[:innerHTML]
+p main[:innerHTML].class
 
 # これ無理
-# p main.innerHTML
+p main.innerHTML
+p main.innerHTML.class

5b3118799d7d1f32cc53d8f61f2d922fb4a58db6
ongaeshi <ongaeshi0621@gmail.com>
2023-02-19 16:59:11 +0900

    refactor: Use instance variable

--- docs/examples/structure-create-graphics/main.rb
+++ docs/examples/structure-create-graphics/main.rb
@@ -1,8 +1,8 @@
-$pg = nil
+@pg = nil
 
 def setup
   createCanvas(710, 400)
-  $pg = createGraphics(400, 250)
+  @pg = createGraphics(400, 250)
 end
 
 def draw
@@ -12,11 +12,11 @@ def draw
   noStroke()
   ellipse(mouseX, mouseY, 60, 60)
 
-  $pg.background(51)
-  $pg.noFill()
-  $pg.stroke(255)
-  $pg.ellipse(mouseX - 150, mouseY - 75, 60, 60)
+  @pg.background(51)
+  @pg.noFill()
+  @pg.stroke(255)
+  @pg.ellipse(mouseX - 150, mouseY - 75, 60, 60)
 
   #Draw the offscreen buffer to the screen with image()
-  image($pg, 150, 75)
+  image(@pg, 150, 75)
 end

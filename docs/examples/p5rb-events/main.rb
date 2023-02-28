44fb746ce3928dcd973dc123ccf4a1ec5f127523
ongaeshi <ongaeshi0621@gmail.com>
2023-03-01 01:16:31 +0900

    feat: Add all mouse event samples

--- docs/examples/p5rb-events/main.rb
+++ docs/examples/p5rb-events/main.rb
@@ -1,4 +1,6 @@
-$positions = []
+$circles = []
+$rects = []
+$bgcolor = 102
 
 def preload
   p "preload"
@@ -10,20 +12,50 @@ def setup
 end
 
 def draw
-  background(102)
+  background($bgcolor)
 
-  $positions.each do |pos|
+  fill(255)
+  $circles.each do |pos|
     circle(pos[:x], pos[:y], 5)
   end
-end
 
-def mousePressed(e)
-  p e.shiftKey
-  $positions << {x: mouseX, y: mouseY}
+  fill(128, 128, 128, 255)
+  $rects.each do |pos|
+    rect(pos[:x], pos[:y], 20)
+  end
 end
 
 def mouseMoved(e)
   p e
+end
+
+def mouseDragged(e)
+  $bgcolor = $bgcolor + 5
+  $bgcolor = 0 if $bgcolor > 255
+end
+
+def mousePressed(e)
+  p e.shiftKey
+  $circles << {x: mouseX, y: mouseY}
+end
+
+def mouseReleased(e)
+  puts "mouseReleased #{e}"
+end
+
+def mouseClicked(e)
+  puts "mouseClicked #{e}"
+end
+
+def doubleClicked(e)
+  $rects << {x: mouseX, y: mouseY}
+end
+
+def mouseWheel(e)
+  p e.delta
+  $bgcolor += e.delta * 0.2
+  $bgcolor = 0 if $bgcolor > 255
+  $bgcolor = 255 if $bgcolor < 0
 end
 
 def keyPressed(e)
